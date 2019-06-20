e87febd0cc31a79c3532c5cf58c1cfecb8248d71
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-19 21:49:51 -0400

    Randomly select number of tiles in x and y directions and use these to calculate width/height of each tile

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 // Snippet from https://codepen.io/Halfhappy/pen/DmxuB
 const random_color = () => '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
 
-for (var i = 0; i < 10; i ++) {
-	col = $('<div>').css('width', '10%').css('height', '100%').addClass('col');
-	for (var j = 0; j < 10; j ++) {
+x_num = Math.ceil(Math.random() * 15) + 5;
+y_num = Math.ceil(Math.random() * 5) + 5;
+
+for (var i = 0; i < x_num; i ++) {
+	col = $('<div>').css('width', 100 / x_num + '%').css('height', '100%').addClass('col');
+	for (var j = 0; j < y_num; j ++) {
 		col.append(
-			$('<div data-tilt></div>').css('width', '100%').css('height', '10%').addClass('tile').css('background-color', random_color())
+			$('<div data-tilt></div>').css('width', '100%').css('height', 100 / y_num + '%').addClass('tile').css('background-color', random_color())
 		);
 	}
 	$('#grid').append(col);
